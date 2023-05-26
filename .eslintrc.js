ea2669fb4054127da09082080f243054b15047f5
George <lame.giorgos@gmail.com>
2023-05-26 11:21:30 +0300

    Gamw to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended",
     // 'plugin:prettier/recommended',
     // 'plugin:react-hooks/recommended',
     // 'plugin:storybook/recommended',
